719117029133ae7b9e51593b88aa38fdae535d7d
Ahmed Bankole <thegeekyahmed@gmail.com>
2020-08-30 00:02:50 +0100

    fixed await err and added routes

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -2,13 +2,15 @@ const Post = require('../models/post')
 const Notification = require('../models/notification')
 
 module.exports = {
-    getPosts: async (req, res) => {
-        await Post.find({ isPublished: true })   
-            .then(posts => {
-                await res.status(200).json(posts)
-            })
-            .catch(err => {
-            await res.status(500).json({ msg: "Internal Server Error", error: err.message })
-        })
-    }
+  getPosts: async (req, res) => {
+    await Post.find({ isPublished: true })
+      .then(posts => {
+        res.status(200).json(posts)
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ msg: 'Internal Server Error', error: err.message })
+      })
+  }
 }
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const connectDB = require('./config/db')
+const apiRoutes = require('./routes/api.routes')
 const port = require('./config/port')
 const app = express()
 
@@ -12,6 +13,8 @@ app.use(
     extended: false
   })
 )
+
+app.use('/api', apiRoutes)
 
 app.listen(port.number, err => {
   console.log('API Live At Port ' + `${port.number}`)
