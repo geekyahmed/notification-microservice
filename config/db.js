3704ecad30a81f19003cb157cec3121116b3f7d8
Ahmed Bankole <thegeekyahmed@gmail.com>
2020-08-29 23:21:16 +0100

    setup db connection and import to server

--- config/db.js
+++ config/db.js
@@ -1,12 +1,27 @@
-require("dotenv").config();
+require('dotenv').config()
+const mongoose = require('mongoose')
 
 const db = {
-    development: {
-        uri: process.env.MONGODB_URi
-    },
-    production: {
-        uri: process.env.MONGOATLAS_URi
-    }
+  uri: process.env.MONGODB_URI
+}
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  reconnectTries: 60,
+  reconnectInterval: 1000,
+  poolSize: 10,
+  bufferMaxEntries: 0 // If not connected, return errors immediately rather than waiting for reconnect
 }
 
-module.exports = db;
+const connectDB = () => {
+  mongoose
+    .connect(db.uri, options)
+    .then(response => {
+      console.log(`MongoDB Connected Successfully`)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+}
+
+module.exports = connectDB
--- server.js
+++ server.js
@@ -1,27 +1,18 @@
 const express = require('express')
-const mongoose = require('mongoose')
-const {
-  db
-} = require('./config/db')
+const connectDB = require('./config/db')
 const port = require('./config/port')
-
 const app = express()
+
+connectDB()
 
 app.use(express())
 app.use(express.json())
-app.use(express.urlencoded({
-  extended: true
-}))
-
-const options = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  reconnectTries: 60,
-  reconnectInterval: 1000,
-  poolSize: 10,
-  bufferMaxEntries: 0 // If not connected, return errors immediately rather than waiting for reconnect
-};
+app.use(
+  express.urlencoded({
+    extended: false
+  })
+)
 
 app.listen(port.number, err => {
-  console.log('Server running at' + `${port.number}`)
+  console.log('API Live At Port ' + `${port.number}`)
 })
